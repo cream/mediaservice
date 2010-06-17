187aa731acbe99226ec1213e16e5d799147a291d
kkris <kris.kleine@yahoo.de>
2010-06-17 22:25:09 +0200

    added signal, using other collection

--- mediaservice/audio/audioservice.py
+++ mediaservice/audio/audioservice.py
@@ -9,6 +9,12 @@ from crawler import crawl
 
 @cream.extensions.register
 class AudioExtension(cream.extensions.Extension, cream.ipc.Object):
+
+    __ipc_signals__ = {
+        'library_updated': ('', 'org.cream.Mediaservice.Audio')
+        }
+
+
     def __init__(self, *args, **kwargs):
         cream.extensions.Extension.__init__(self, *args, **kwargs)
         cream.ipc.Object.__init__(self,
@@ -20,14 +26,17 @@ class AudioExtension(cream.extensions.Extension, cream.ipc.Object):
 
     @cream.ipc.method('s', interface='org.cream.Mediaservice.Audio')
     def update_library(self, path):
-        crawl(self.collection, path)
+        crawl(self.collection.tracks, path)
+        self.emit_signal('library_updated')
+
 
     @cream.ipc.method('', 'aa{sv}', interface='org.cream.Mediaservice.Audio')
     def list_tracks(self):
         tracks = []
-        for track in self.collection.find():
+        for track in self.collection.tracks.find():
             tracks.append(format_id(track))
         return tracks
+
 
     @cream.ipc.method('', 'aa{sv}', interface='org.cream.Mediaservice.Audio')
     def query(self, query):
@@ -35,7 +44,7 @@ class AudioExtension(cream.extensions.Extension, cream.ipc.Object):
             query[key] = re.compile(query[key], re.IGNORECASE)
 
         tracks = []
-        for track in self.collection.find(query):
+        for track in self.collection.tracks.find(query):
             tracks.append(format_id(track))
         return tracks
 
