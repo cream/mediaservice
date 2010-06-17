b3823248da02144ddd8010be2f3dd2efa4607458
kkris <kris.kleine@yahoo.de>
2010-06-17 20:11:52 +0200

    sending over dbus works now

--- mediaservice/audio/audioservice.py
+++ mediaservice/audio/audioservice.py
@@ -16,13 +16,25 @@ class AudioExtension(cream.extensions.Extension, cream.ipc.Object):
 
         self.collection = args[0].audio
 
-    @cream.ipc.method(interface='org.cream.Mediaservice.Audio')
+    @cream.ipc.method('', 'aa{sv}', interface='org.cream.Mediaservice.Audio')
     def list_tracks(self):
-        return [track for track in self.collection.find()]
+        tracks = []
+        for track in self.collection.find():
+            tracks.append(format_id(track))
+        return tracks
 
-    @cream.ipc.method(interface='org.cream.Mediaservice.Audio')
+    @cream.ipc.method('', 'aa{sv}', interface='org.cream.Mediaservice.Audio')
     def query(self, query):
         for key in query.keys():
             query[key] = re.compile(query[key], re.IGNORECASE)
 
-        return [track for track in self.collection.find(query)]
+        tracks = []
+        for track in self.collection.find(query):
+            tracks.append(format_id(track))
+        return tracks
+
+
+
+def format_id(dict_):
+    dict_['_id'] = str(dict_['_id'])
+    return dict_
